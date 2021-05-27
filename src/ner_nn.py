6e705035e596b9ab803407159cee491de5a07b99
alex <alex.llobet@estudiantat.upc.edu>
2021-05-27 19:11:38 +0200

    Affixes included. 57.9% achieved.

--- src/ner_nn.py
+++ src/ner_nn.py
@@ -1,15 +1,24 @@
-from src.utils.data_generator import DatasetGenerator
-from keras.models import Model, Input, load_model
-from keras.initializers import he_normal
-from keras import optimizers
-from keras.layers import LSTM, Embedding, Dense, TimeDistributed, Bidirectional
-from keras.utils import to_categorical
-from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.layers import Concatenate
+
+from utils.data_generator import DatasetGenerator
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.layers import Input
+from tensorflow.keras.initializers import he_normal
+from tensorflow.keras import optimizers
+from tensorflow.keras.layers import LSTM, Embedding, Dense, TimeDistributed, Bidirectional
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from src.utils.utility import print_sentences_len_hist, plot_training
 import numpy as np
+# from keras_contrib.layers import CRF
 import json
+from nltk.corpus import stopwords as stopwords
 from eval import evaluator
 
+
+# Define global attributes
+LEN_AFFIX = 3
+sw = stopwords.words('english')
 
 def learn(train_dir, val_dir, model_name=None):
     train_data = load_data(train_dir)
@@ -25,10 +34,14 @@ def learn(train_dir, val_dir, model_name=None):
     optimizer = optimizers.Adam(learning_rate=0.01)
     model = build_network(indexes, optimizer)
 
-    X_train = encode_words(train_data, indexes)
+    prefixes_encoded, suffixes_encoded = encode_affixes(train_data, indexes)
+    X_train = [encode_words(train_data, indexes),prefixes_encoded,suffixes_encoded]
     y_train = encode_labels(train_data, indexes)
-    X_val = encode_words(val_data, indexes)
+
+    prefixes_encoded, suffixes_encoded = encode_affixes(val_data, indexes)
+    X_val = [encode_words(val_data, indexes),prefixes_encoded,suffixes_encoded]
     y_val = encode_labels(val_data, indexes)
+
 
     # TODO note: My pc cannot allow setting steps_per_epochs and validation_steps...
     # Better focus the training of maximizing the reduction of val loss --> better generalization!
@@ -49,7 +62,10 @@ def learn(train_dir, val_dir, model_name=None):
 def predict(model_name, data_dir):
     model, indexes = load_model_and_indexes(model_name)
     test_data = load_data(data_dir)
-    X_test = encode_words(test_data, indexes)
+    encoded_words = encode_words(test_data, indexes)
+    encoded_preffixes, encoded_suffixes = encode_affixes(test_data, indexes)
+
+    X_test = [encoded_words, encoded_preffixes, encoded_suffixes]
     y_pred = model.predict(X_test, verbose=1)
     # get most likely tag for each word
     pred_labels = y_pred_to_labels(y_pred, indexes)
@@ -69,25 +85,60 @@ def create_indexes(train_data, max_len=100):
                   'labels': {'<PAD>': 0, 'O': 1, 'B-drug': 2, 'I-drug': 3,
                         'B-group': 4, 'I-group': 5,  'B-brand': 6, 'I-brand': 7,
                         'B-drug_n': 8, 'I-drug_n': 9},
+                  'suffixes': {'<PAD>': 0, '<UNK>':1},
+                  'prefixes': {'<PAD>': 0, '<UNK>': 1},
                   'maxLen': max_len}
     word_index = 2
+    suf_index = 2
+    pref_index = 2
     word_dict = index_dict['words']
+    suffix_dict = index_dict['suffixes']
+    prefix_dict = index_dict['prefixes']
     for instances_of_a_sentence in train_data.values():
         for instance in instances_of_a_sentence:
             word = instance[0]
             if word not in word_dict:
                 word_dict[word] = word_index
                 word_index += 1
+
+            # get the 3 length suffix/prefix if it is not a stop word.
+            if word not in sw and len(word) > LEN_AFFIX:
+                suffix = word[-LEN_AFFIX:]
+                prefix = word[:LEN_AFFIX]
+                if suffix not in suffix_dict:
+                    suffix_dict[suffix] = suf_index
+                    suf_index += 1
+                if prefix not in prefix_dict:
+                    prefix_dict[prefix] = pref_index
+                    pref_index += 1
+                #suffix_dict[suffix] = suffix_dict.get(suffix, len(suffix_dict)) + 1
+                #prefix_dict[prefix] = prefix_dict.get(prefix, len(prefix_dict)) + 1
+
+
     return index_dict
 
 
 def build_network(indexes, optimizer):
     n_words = len(indexes['words'])
+    n_suff = len(indexes['suffixes'])
+    n_pref = len(indexes['prefixes'])
     n_labels = len(indexes['labels'])
+
     max_len = indexes['maxLen']
+
     word_embedding_size = max_len - int(max_len*0.1)  # max sentence len + 10%
-    input = Input(shape=(max_len,))
-    model = Embedding(input_dim=n_words,  output_dim=word_embedding_size, input_length=max_len, mask_zero=True)(input)
+    suffix_embedding_size = word_embedding_size # for now they have the same length
+    prefix_embedding_size = word_embedding_size
+    # 3 input layers, one for each feature
+    input_words = Input(shape=(max_len,))
+    input_prefixes = Input(shape=(max_len,))
+    input_suffixes = Input(shape=(max_len,))
+    # 3 embeddings (one for each input)
+    word_emb = Embedding(input_dim=n_words,  output_dim=word_embedding_size, input_length=max_len, mask_zero=True)(input_words)
+    pref_emb = Embedding(input_dim=n_pref, output_dim=prefix_embedding_size, input_length=max_len, mask_zero=True)(input_prefixes)
+    suff_emb = Embedding(input_dim=n_suff, output_dim=suffix_embedding_size, input_length=max_len, mask_zero=True)(input_suffixes)
+    # concatenate embeddings and feed the biLSTM model
+    model = Concatenate()([word_emb, pref_emb, suff_emb])
     model = Bidirectional(LSTM(units=word_embedding_size, return_sequences=True, recurrent_dropout=0.1, dropout=0.1,
                                kernel_initializer=he_normal()))(model)
     # model = LSTM(units=word_embedding_size * 2,
@@ -96,7 +147,7 @@ def build_network(indexes, optimizer):
     #              recurrent_dropout=0.1,
     #              kernel_initializer=he_normal())(model)
     out = TimeDistributed(Dense(n_labels, activation="softmax"))(model)
-    model = Model(input, out)
+    model = Model([input_words, input_prefixes, input_suffixes], out)
     model.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy"])
     return model
 
@@ -106,6 +157,8 @@ def encode_words(split_data, indexes):
     max_len = indexes['maxLen']
     encoded_matrix = []
     for instances_of_a_sentence in split_data.values():
+        #pos_tags_of_sentence = pos_tag([token[0] for token in instances_of_a_sentence])
+
         encoded_sentence = []
         for idx, instance in enumerate(instances_of_a_sentence):
             # If the sentence is bigger than max_len we need to cut the sentence
@@ -125,6 +178,64 @@ def encode_words(split_data, indexes):
         encoded_matrix.append(encoded_sentence)
     return np.array(encoded_matrix)
 
+def encode_affixes(split_data, indexes) -> tuple:
+    # load both dictionaries. Instantiate the lists which will contain all the encoded sentences for
+    # both prefix and suffix policies.
+
+    suffix_dict = indexes['suffixes']
+    prefix_dict = indexes['prefixes']
+    max_len = indexes['maxLen']  # TODO: try different max lentgths for both approaches
+    encoded_dataset_suf = []
+    encoded_dataset_pre = []
+
+    for instances_of_a_sentence in split_data.values():
+        encoded_sentence_suf = []
+        encoded_sentence_pref = []
+        # words that do not have the required length or are stopwords will be filtered out.
+        # TODO: (solved) better just put a <UNK>
+
+        for idx, instance in enumerate(instances_of_a_sentence):
+            # If the sentence is bigger than max_len we need to cut the sentence
+            if idx < max_len:
+                word = instance[0]
+                # words that do not have the required length or are stopwords have <UNK>
+                if len(word) < LEN_AFFIX:
+                    encoded_sentence_suf.append(suffix_dict['<UNK>'])
+                    encoded_sentence_pref.append(prefix_dict['<UNK>'])
+                    continue
+
+                # beyond this point, words are adequate to extract affixes.
+
+                suffix = word[-LEN_AFFIX:]
+                prefix = word[:LEN_AFFIX]
+
+                if suffix in suffix_dict:
+                    encoded_sentence_suf.append(suffix_dict[suffix])
+                else:  # '<UNK>' : 1
+                    encoded_sentence_suf.append(suffix_dict['<UNK>'])
+
+                if prefix in prefix_dict:
+                    encoded_sentence_pref.append(prefix_dict[prefix])
+                else:  # '<UNK>' : 1
+                    encoded_sentence_pref.append(prefix_dict['<UNK>'])
+            else:
+                break
+        sent_len_suf = len(encoded_sentence_suf)
+        sent_len_pref = len(encoded_sentence_pref)
+        # Check if we need padding
+        if max_len - sent_len_suf > 0:
+            padding = [suffix_dict['<PAD>']] * (max_len - sent_len_suf)
+            encoded_sentence_suf.extend(padding)
+
+        if max_len - sent_len_pref > 0:
+            padding = [prefix_dict['<PAD>']] * (max_len - sent_len_pref)
+            encoded_sentence_pref.extend(padding)
+
+
+        encoded_dataset_suf.append(encoded_sentence_suf)
+        encoded_dataset_pre.append(encoded_sentence_pref)
+
+    return (np.array(encoded_dataset_pre), np.array(encoded_dataset_suf))
 
 def encode_labels(split_data, indexes):
     label_dict = indexes['labels']
